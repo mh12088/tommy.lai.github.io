0380f1cc4c862bd4692cb9bca0aac7f3b6f13152
tommy.lai <tommy@aishk.com>
2021-11-23 18:08:50 +0800

    Config ga service

---
+++ src/analytics-helper.js
@@ -0,0 +1,24 @@
+const measurementId = `G-TFJ9H2W5Q3`;
+const apiSecret = `PLd3UejWQziRFCvzqyToiA`;
+// const measurementId = `G-WG3DE9SEBK`;
+// const apiSecret = `ox2CicStRTCDcJEc_snDDg`;
+
+const sendAnalyticsEvent = (eventName, eventValue) => {
+
+  console.log('Sending analytics event');
+
+  return fetch(`https://www.google-analytics.com/mp/collect?measurement_id=${measurementId}&api_secret=${apiSecret}`, {
+    method: "POST",
+    body: JSON.stringify({
+      "client_id": "client_id",
+      "events": [{
+        "name": eventName,
+        "params": {
+          "url": eventValue,
+        }
+      }]
+    })
+  });
+};
+
+// This file is for pwa on service worker send event to ga4

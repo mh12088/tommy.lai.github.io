fd7151d1c8c1b3a45adf7e936c6c8d4a84ab6f06
tommy.lai <tommy@aishk.com>
2022-01-05 15:32:41 +0800

    Auto-generated commit [ci skip]

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,30 +1,29 @@
-// Scripts for firebase and firebase messaging
-importScripts("https://www.gstatic.com/firebasejs/8.2.0/firebase-app.js");
-importScripts("https://www.gstatic.com/firebasejs/8.2.0/firebase-messaging.js");
+importScripts('https://www.gstatic.com/firebasejs/8.0.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.0.0/firebase-messaging.js');
 
-// Initialize the Firebase app in the service worker by passing the generated config
-const firebaseConfig = {
-    apiKey: "AIzaSyAsXuRCH_wOBveI6B__7s8RxKwepkOHSYs",
-    authDomain: "webauthn-demo-b0943.firebaseapp.com",
-    projectId: "webauthn-demo-b0943",
-    storageBucket: "webauthn-demo-b0943.appspot.com",
-    messagingSenderId: "459550575359",
-    appId: "1:459550575359:web:0da6a3c92e12751c9e9aca",
-};
-
-firebase.initializeApp(firebaseConfig);
-
-// Retrieve firebase messaging
-const messaging = firebase.messaging();
-
-messaging.onBackgroundMessage(function (payload) {
-    console.log("Received background message ", payload);
-
-    const notificationTitle = payload.notification.title;
-    const notificationOptions = {
-        body: payload.notification.body,
-    };
-
-    self.registration.showNotification(notificationTitle, notificationOptions);
+firebase.initializeApp({
+  apiKey: "AIzaSyAsXuRCH_wOBveI6B__7s8RxKwepkOHSYs",
+  authDomain: "webauthn-demo-b0943.firebaseapp.com",
+  projectId: "webauthn-demo-b0943",
+  storageBucket: "webauthn-demo-b0943.appspot.com",
+  messagingSenderId: "459550575359",
+  appId: "1:459550575359:web:0da6a3c92e12751c9e9aca",
 });
 
+const messaging = firebase.messaging();
+
+getToken(messaging, { vapidKey: "BDR7cEU2X-gx7thlDr9N28pzrpdauh-V4y508e45jnV8Mpj4ezYQDf-PveDkHLwg5P_n96Yo8W51ZWThDyConBE" }).then((currentToken) => {
+  if (currentToken) {
+    // Send the token to your server and update the UI if necessary
+    // ...
+  } else {
+    // Show permission request UI
+    console.log('No registration token available. Request permission to generate one.');
+    // ...
+  }
+}).catch((err) => {
+  console.log('An error occurred while retrieving token. ', err);
+  // ...
+});
+
+getToken();

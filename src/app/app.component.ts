164144ab05edcbf472531c922aa6d62b3afc9626
tommy.lai <tommy@aishk.com>
2022-01-19 16:24:10 +0800

    Add firebase config for push notification

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { GoogleAnalyticsService } from './pwa-install-promotion/service/google-analytics-service';
 import * as uuid from 'uuid';
+import { initializeApp } from "firebase/app";
+import { getMessaging, getToken, onMessage } from "firebase/messaging";
+import { FireBaseService } from './push-notification/service/firebase.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,7 +13,7 @@ import * as uuid from 'uuid';
 export class AppComponent implements OnInit {
   title = 'biometric-login-demo';
 
-  constructor(private _$gaService: GoogleAnalyticsService) { }
+  constructor(private _$gaService: GoogleAnalyticsService, private _$firebaseService: FireBaseService) { }
 
   ngOnInit() {
     this._$gaService.init();
@@ -17,5 +21,7 @@ export class AppComponent implements OnInit {
     if (!localStorage.getItem("device_id")) {
       localStorage.setItem("device_id", deviceId);
     };
+
+    this._$firebaseService.init();
   }
 }
--- src/app/push-notification/service/firebase.service.ts
+++ src/app/push-notification/service/firebase.service.ts
@@ -1,22 +1,16 @@
 import { Injectable } from '@angular/core';
-import { SwPush, SwUpdate } from '@angular/service-worker';
 import { initializeApp } from "firebase/app";
-import { getMessaging } from "firebase/messaging/sw";
-import { getToken } from "firebase/messaging";
-declare var gtag: any;
+import { getMessaging, getToken, onMessage } from "firebase/messaging";
 @Injectable({
     providedIn: 'root'
 })
 export class FireBaseService {
 
-    constructor(private swPush: SwPush, private swUpdate: SwUpdate) {
-        swUpdate.available.subscribe(() => {
-            window.location.reload();
-        });
+    constructor() {
     }
 
     init() {
-        const firebaseConfig = {
+        const firebaseApp = initializeApp({
             apiKey: "AIzaSyAsXuRCH_wOBveI6B__7s8RxKwepkOHSYs",
             authDomain: "webauthn-demo-b0943.firebaseapp.com",
             projectId: "webauthn-demo-b0943",
@@ -24,11 +18,11 @@ export class FireBaseService {
             messagingSenderId: "459550575359",
             appId: "1:459550575359:web:0da6a3c92e12751c9e9aca",
             measurementId: "G-5SX5JWR775"
-        };
+        });
 
         // Initialize Firebase
-        const app = initializeApp(firebaseConfig);
-        const messaging = getMessaging(app);
+        const messaging = getMessaging(firebaseApp);
+
         getToken(messaging, { vapidKey: 'BDR7cEU2X-gx7thlDr9N28pzrpdauh-V4y508e45jnV8Mpj4ezYQDf-PveDkHLwg5P_n96Yo8W51ZWThDyConBE' }).then((currentToken) => {
             if (currentToken) {
                 // Send the token to your server and update the UI if necessary
@@ -44,8 +38,8 @@ export class FireBaseService {
             // ...
         });
 
-        this.swPush.messages.subscribe(message => {
-            console.log(message);
-        })
+        onMessage(messaging, (payload) => {
+            console.log('Received foreground message: ', payload);
+        });
     }
 }
